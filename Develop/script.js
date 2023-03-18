c227469eb0d807471e082c0a1684ac21850df620
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2023-03-17 20:45:07 -0700

    3/17 work

--- Develop/script.js
+++ Develop/script.js
@@ -23,32 +23,41 @@ $(".slot").each(function (i, value) {
 //current hour
 console.log(moment().format("HH"));
 
-//loop through textareaS, for each textarea, get the id value, and compare to current time
-
-//event.target or 'this' key word
-//traverse the DOM : to text area and store value of that text area
 
 // document.getElementById("saveBtn").addEventListener("click", saveInput());
 
-function saveInput() {
-  console.log(document.getElementById.previousElementSibling);
-}
+//TODO: create an object using JQUERY
+// store each object to LS 
+// parse in from the block ID, change the text area for that block to the stored texIn
+var index = $('<textarea>')[0];
+var textInput = $('<textarea>');
+
+// function renderItems(){
+//     //set each textarea content to the value stored in the LS for that specific TA id
+//     // $("saveBtn").previousElementSibling.value = "save";
+// };
+
+
+// function saveInput() {
+//   console.log(document.getElementById.previousElementSibling);
+// }
+
+// $(".saveBtn").on("click", function (event) {
+//   event.preventDefault();
+//   // var textValue = $(this).prev().val()
+//   var textValue = $(this).prev().val().trim();
+//   console.log(textValue);
+//   var index = event.target.previousElementSibling.getAttribute("id");
+//   console.log(index);
+//     //save textValue to locastorage
+//  localStorage.setItem(JSON.stringify(index), JSON.stringify(textValue))
+// });
 
 $(".saveBtn").on("click", function (event) {
-  event.preventDefault();
-  // var textValue = $(this).prev().val()
-  var textValue = $(this).prev().val().trim();
-  console.log(textValue);
-  var index = event.target.previousElementSibling.getAttribute("id");
-  console.log(index);
-    //save textValue to locastorage
- localStorage.setItem(JSON.stringify(index), JSON.stringify(textValue)); // $(this).textContent = textValue;
-
-});
-
-//loop through each textareaa
-//each textarea, display value that you got from local storage
-
-//get textvalue from localstorage
-//and idsplay on each text area
-var savedInp = localStorage.getItem("inputTxt");
+    event.preventDefault();
+    var task = {
+        hour: index,
+        textIn: textInput.value,
+    };
+   localStorage.setItem("task", JSON.stringify(task));
+  });
